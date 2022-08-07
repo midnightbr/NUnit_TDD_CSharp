c34942c6244e40c611d9a44b3ba1693e4c22af97
marcos.henrique <marcos.henrique@engegraph.com.br>
2022-08-07 01:18:33 -0300

    Aula 2 - Conceitos de testes Assets do NUnit Mostrando algumas opções para a execução do teste, além de evitar alguns detalhes caso não seja necessario.

---
+++ Demo_Asserts/Demo_Asserts.Tests/Igualdade/JuntarNomesTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace Demo_Asserts.Tests.Igualdade
+{
+    [TestFixture]
+    public class JuntarNomesTests
+    {
+        /* Método responsavel por juntar as strings*/
+        [Test]
+        public void DevoJuntarNomeCompleto()
+        {
+            var sut = new JuntarNome();
+
+            var nomeCompleto = sut.Juntar("John", "Lucca");
+            Assert.That(nomeCompleto, Is.EqualTo("John Lucca"));
+        }
+
+        [Test]
+        public void DevoJuntarNomeCompleto_CaseSensitive()
+        {
+            var sut = new JuntarNome();
+
+            var nomeCompleto = sut.Juntar("marcos", "Henrique");
+
+            // O IgnoreCase ira ignorar do resultar a letras maiusculas e minusculas, como no exemplo a baixo
+            Assert.That(nomeCompleto, Is.EqualTo("MARCOS HENRIQUE").IgnoreCase);
+        }
+
+        [Test]
+        public void DevoJuntarNomeCompleto_NaoIgual()
+        {
+            var sut = new JuntarNome();
+
+            var nomeCompleto = sut.Juntar("John", "Lucca");
+
+            // Método para verificar se o resulta é diferente, ou seja, não é igual
+            Assert.That(nomeCompleto, Is.Not.EqualTo("Fabiana Miranda"));
+        }
+    }
+}
---
+++ Demo_Asserts/Demo_Asserts/JuntarNome.cs
@@ -0,0 +1,11 @@
+﻿namespace Demo_Asserts
+{
+    public class JuntarNome
+    {
+        /* Método principal*/
+        public string Juntar(string nome, string sobrenome)
+        {
+            return $"{nome} {sobrenome}";
+        }
+    }
+}
