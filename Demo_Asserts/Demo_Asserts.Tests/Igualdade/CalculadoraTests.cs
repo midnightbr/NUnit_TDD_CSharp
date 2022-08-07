a3c3beebac739b21414f60261574fb6161a56125
marcos.henrique <marcos.henrique@engegraph.com.br>
2022-08-07 02:59:32 -0300

    Aula 03 Demonstrado sobre algumas operações possiveis de serem feitas com os numeros dentro dos testes, além de poder fazer teste se a execução do método retorna algum erro.

---
+++ Demo_Asserts/Demo_Asserts.Tests/Igualdade/CalculadoraTests.cs
@@ -0,0 +1,73 @@
+﻿using NUnit.Framework;
+
+namespace Demo_Asserts.Tests.Igualdade
+{
+    [TestFixture]
+    public class CalculadoraTests
+    {
+        // Método para Somar Inteiros
+        [Test]
+        public void TesteSomarInteiro()
+        {
+            var sut = new Calculadora();
+
+            var result = sut.SomarNumerosInteiros(25, 10);
+
+            Assert.That(result, Is.EqualTo(35));
+        }
+
+        // Método para Somar Decimais
+        [Test]
+        public void TesteSomarDecimal()
+        {
+            var sut = new Calculadora();
+
+            var result = sut.SomarNumerosDecimais(7.5, 10.8);
+
+            Assert.That(result, Is.EqualTo(18.3));
+        }
+
+        // Resolvendo problemas com pontos flutuantes
+        [Test]
+        public void TesteSomarDecimal_UsandoWithin()
+        {
+            var sut = new Calculadora();
+
+            var result = sut.SomarNumerosDecimais(1.1, 2.2);
+
+            Assert.That(result, Is.EqualTo(3.3).Within(0.01)); 
+            // Within é a tolerancia de erro aceitado pelo teste (margem de erro, vamos chamar assim)
+        }
+
+        [Test]
+        public void TesteSomarDecimal_UsandoWithinPorcentagem()
+        {
+            var sut = new Calculadora();
+
+            var result = sut.SomarNumerosDecimais(50, 50);
+
+            Assert.That(result, Is.EqualTo(101).Within(1).Percent);
+            // Whithin().Percent informa a tolerancia aceitavel em porcetagem, não em valor
+        }
+
+        // Método para Dividir
+        [Test]
+        public void TesteDividir()
+        {
+            var sut = new Calculadora();
+
+            var result = sut.Dividir(50, 20);
+
+            Assert.That(result, Is.EqualTo(2.5));
+        }
+
+        [Test]
+        public void TesteDividirErro()
+        {
+            var sut = new Calculadora();
+
+            // Testando se o resultado da operação retorna um erro
+            Assert.Throws<Exception>(() => sut.Dividir(150, 50));
+        }
+    }
+}
---
+++ Demo_Asserts/Demo_Asserts/Calculadora.cs
@@ -0,0 +1,25 @@
+﻿namespace Demo_Asserts
+{
+    public class Calculadora
+    {
+        public int SomarNumerosInteiros(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public double SomarNumerosDecimais(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public double Dividir(double number1, double number2)
+        {
+            if (number1 > 100)
+            {
+                throw new Exception("por"); // propositos para a demo
+            }
+
+            return number1 / number2;
+        }
+    }
+}
