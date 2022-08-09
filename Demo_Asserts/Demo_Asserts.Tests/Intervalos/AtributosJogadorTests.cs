32f02975e728b6aa14e32ef0e41470181022e57b
marcos.henrique <marcos.henrique@engegraph.com.br>
2022-08-08 21:40:32 -0300

    Aula testando intervalos de valores

---
+++ Demo_Asserts/Demo_Asserts.Tests/Intervalos/AtributosJogadorTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+
+namespace Demo_Asserts.Tests.Intervalos
+{
+    [TestFixture]
+    public class AtributosJogadorTests
+    {
+        /*
+         * Is.LessThan() -> Verficia se é menor que o valor informado
+         * Is.LessThanOrEqualTo() -> Verficia se é menor ou igual que o valor informado
+         * Is.GreaterThan() -> Verficia se é maior que o valor informado
+         * Is.GreaterThanOrEqualTo() -> Verficia se é maior que o valor informado
+         */
+
+        [Test]
+        public void AumentarVidaJogadorDepoisDormir()
+        {
+            var sut = new AtributosJogador()
+            {
+                Vida = 100
+            };
+
+            sut.Dormir();
+
+            // Testa se o valor atual é maior do que o argumento dado
+            Assert.That(sut.Vida, Is.GreaterThan(100));
+        }
+
+        [Test]
+        public void AumentarVidaJogadorDepoisDormir_Intervalo()
+        {
+            var sut = new AtributosJogador()
+            {
+                Vida = 100
+            };
+
+            sut.Dormir();
+
+            // Essa opção verifica se o valor de sut.Vida está dentro do intervalo de 101 á 200
+            Assert.That(sut.Vida, Is.InRange(101, 200));
+        }
+    }
+}
---
+++ Demo_Asserts/Demo_Asserts/AtributosJogador.cs
@@ -0,0 +1,59 @@
+﻿namespace Demo_Asserts
+{
+    public class AtributosJogador
+    {
+        public int Vida { get; set; }
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        public bool NovoJogador { get; set; }
+        public List<string> Armas { get; set; }
+
+        public AtributosJogador()
+        {
+            Nome = GerarNome();
+
+            NovoJogador = true;
+
+            CriarArmasIniciais();
+        }
+
+        // Método para aumentar a vida do Jogador
+        public void Dormir()
+        {
+            var random = new Random();
+
+            Vida += random.Next(1, 100);
+        }
+
+        // Método para perder vida do jogador
+        public void PerderVida(int perderVida) => Vida = Math.Max(1, Vida -= perderVida);
+
+        private void CriarArmasIniciais()
+        {
+            Armas = new List<string>()
+            {
+                "MA40 Assault Rifle",
+                "BR75 Battle Rifle",
+                "VK78 Commando",
+                "Pulse Carbine",
+                "MK 50 Sidekick",
+                "CQS48 Bulldog",
+                "S7 Sniper"
+            };
+        }
+
+        private string GerarNome()
+        {
+            var nomes = new[]
+            {
+                "Master-Chief",
+                "Cortana",
+                "Kai",
+                "Jacoob",
+                "Riz"
+            };
+
+            return nomes[new Random().Next(0, nomes.Length)];
+        }
+    }
+}
